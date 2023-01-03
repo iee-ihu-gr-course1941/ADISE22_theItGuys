91935519bf309402b079340758491e51656c3bf2
oredomoy <orestisdomoy@gmail.com>
2023-01-03 19:10:39 +0200

    changes in room, home , css_external

--- www/home.php
+++ www/home.php
@@ -18,6 +18,7 @@ if (isset($_SESSION['user'])) {
     <meta charset="UTF-8" />
     <meta http-equiv="X-UA-Compatible" content="IE=edge" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <link rel="stylesheet" href="css_external.css">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous" />
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.1/jquery.min.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js" integrity="sha384-w76AqPfDkMBDXo30jS1Sgez6pr3x5MlQ1ZAGC+nuZB+EYdgRZgiwxhTBTkF7CXvN" crossorigin="anonymous"></script>
@@ -30,6 +31,8 @@ if (isset($_SESSION['user'])) {
     <div class="container-fluid bg-image">
         <div id="showAvaibleRooms">
             <div class="row">
+            <div class="col-md-6 col-sm-1 col-lg-12">
+            <div class="p-3 mb-2 bg-info text-white">
                 <span class="d-flex justify-content-between">
                     <span class="d-flex gap-1">
                         <h2 class="text-center">Available Rooms:</h2>
@@ -37,14 +40,15 @@ if (isset($_SESSION['user'])) {
                     </span>
                     <?php
                     if (isset($_SESSION['user'])) {
-                        echo '<h5>userName:  ' . $name . '</h5>';
+                        echo '<h5>Username:  <b><i>' .$name . '</h5></b></i>';
                     }
                     ?>
                 </span>
             </div>
+            </div>
         </div>
     </div>
-
+</div>
     <!-- Modal -->
     <div class="modal" tabindex="-1" id="usernameModal" style="background-color: rgba(0, 0, 0, 0.534)">
         <div class="modal-dialog">
@@ -75,7 +79,7 @@ if (isset($_SESSION['user'])) {
         .bg-image {
             /* The image used */
             background-image: url("homeBg.jpg");
-            height: 100vh;
+            height: 102vh;
             /* Center and scale the image nicely */
             background-size: cover;
         }
@@ -107,13 +111,12 @@ if (isset($_SESSION['user'])) {
                     for (var i = 1; i <= response.records.length; i++) {
                         if (i == 1 || (i - 1) % 3 == 0) $("#showAvaibleRooms").append('<div class="row"></div>');
                         //get last row of page
-                        $(".row:last-child").append('<a href="http://127.0.0.1/ADISE22_theItGuys/www/bluff.php/game/' + response.records[i - 1].id + '" ><div class="box"><h3 class="boxTitle">' + response.records[i - 1].name + "</h3></div></a>");
+                        $(".row:last-child").append('<a style="width:fit-content" href="http://127.0.0.1/ADISE22_theItGuys/www/bluff.php/game/' + response.records[i - 1].id + '" ><div class="box"><h3 class="boxTitle"><h3 style="color:white;text-align:center;">' + response.records[i - 1].name + "</h3></div></a>");
                     }
                     //set avaible rooms
                     $("#totalRooms").text(response.count);
                 },
             });
-
             //set interval every 5 sec to reload avaible rooms
             var intervalId = window.setInterval(function() {
                 if ($("#totalRooms").text() !== "6") {
